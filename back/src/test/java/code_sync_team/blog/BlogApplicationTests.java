d6b4b48e8e065f758dd471f57e2770a4abd5d43a
공태현 <dev0taehyeon@gmail.com>
2024-08-12 21:23:27 +0900

    [init] : Proejct Setting

---
+++ back/src/test/java/code_sync_team/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package code_sync_team.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
